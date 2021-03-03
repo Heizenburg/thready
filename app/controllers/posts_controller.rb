794d5d9af2ed270c0c5b33d4780080d659354a80
Thato Sello <thatokagisosello@gmail.com>
2021-03-03 10:48:47 +0200

    Add files

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+
+  def index
+    @users = User.find_by(params[:id])
+    @posts = Post.all.order("created_at DESC")
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    @post.save
+
+    redirect_to root_path
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to post_url, notice: 'Link was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:comment)
+  end
+end
