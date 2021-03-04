9fb289b19d737e55b70b71f5e41dd604eae58edf
Thato <thatokagisosello@gmail.com>
2021-03-04 07:50:12 +0200

    Add time ago

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to post_url, notice: 'Link was successfully destroyed.' }
+      format.html { redirect_to post_url, notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
