7f7a27e6bad43846f23acffd7f1247a58a66d02d
Thato K Sello <thatokagisosello@gmail.com>
2021-07-13 09:38:25 +0200

    Update errors

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 	root "posts#index"
   resource :posts
   resource :users
+
+  match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 end
